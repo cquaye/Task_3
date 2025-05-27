5b56bd1c2671838b3b8d218de84dd6a96a81e6d5
cquaye <kaptinq@gmail.com>
2025-05-27 11:09:54 -0700

    removed files

--- 0-Authentication.tf
+++
@@ -1,6 +0,0 @@
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs
-provider "google" {
-  project     = "class-6-5-carlton-quaye"
-  region      = "us-central1"
-  credentials = "*"
-}
